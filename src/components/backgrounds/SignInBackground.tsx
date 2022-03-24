3d93951fc46ff78c05c1028992090f9f0a16c9e3
Exp <biellbigama@gmail.com>
2022-03-24 07:15:25 -0300

    feat/Component: input and background image

---
+++ src/components/backgrounds/SignInBackground.tsx
@@ -0,0 +1,15 @@
+import { Image } from "@chakra-ui/react";
+
+export function SignInBackground() {
+  return (
+    <Image
+      position="absolute"
+      width="2000px"
+      objectFit="cover"
+      height="100vh"
+      align="center"
+      src="https://cdnb.artstation.com/p/assets/images/images/024/538/827/original/pixel-jeff-clipa-s.gif?1582740711"
+      alt="Background Spider"
+    />
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,30 @@
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Input,
-  Stack,
-} from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { SignInBackground } from "../components/backgrounds/SignInBackground";
+import { Input } from "../components/Form/Input";
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <Flex w="100vw" h="100vh" align="center" justify="center">
-      <Flex
-        as="form"
-        w="100%"
-        maxWidth={360}
-        bg="gray.800"
-        p="8"
-        borderRadius={8}
-        flexDir="column"
-      >
-        <Stack spacing="4">
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input
-              name="email"
-              type="email"
-              focusBorderColor="blue.300"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: "gray.900",
-              }}
-              size="md"
-            />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="password">Password </FormLabel>
-            <Input
-              name="password"
-              type="password"
-              focusBorderColor="blue.300"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: "gray.900",
-              }}
-              size="md"
-            />
-          </FormControl>
-        </Stack>
-        <Button type="submit" mt="6" colorScheme="blue">
-          Entrar
-        </Button>
+    <>
+      <SignInBackground />
+      <Flex w="100vw" h="100vh" align="center" justify="center">
+        <Flex
+          as="form"
+          w="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDir="column"
+        >
+          <Stack spacing="4">
+            <Input name="email" type="email" label="E-mail" />
+            <Input name="password" type="password" label="senha" />
+          </Stack>
+          <Button type="submit" mt="6" colorScheme="blue">
+            Entrar
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
